838ea0886d4c8ee376dcfcf644414cbf225fc280
Francis Angelo Reyes <81660334+Lupage@users.noreply.github.com>
2021-09-08 12:11:23 +0800

    Update classes.py

--- classes.py
+++ classes.py
@@ -5,6 +5,18 @@ class Page:
     def __init__(self, url):
     	self.url = url
 
+    def page_title(self):
+        page_source = requests.get(self.url).text
+        soup = BeautifulSoup(page_source, 'html.parser')
+        page_title = soup.find('title').get_text()
+        return page_title
+
+    def meta_description(self):
+        page_source = requests.get(self.url).text
+        soup = BeautifulSoup(page_source, 'html.parser')
+        meta_description = soup.find('meta',attrs={'name':'description'})["content"]
+        return meta_description
+
     def word_count(self):
     	page_source = requests.get(self.url).text
     	soup = BeautifulSoup(page_source, 'html.parser')
